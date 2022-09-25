d4274ed77a1e66a3ac32ebe6844f3921cfc12b96
Brandon Sandoval <105913779+brand310@users.noreply.github.com>
2022-09-25 01:55:16 -0700

    Delete functionality implemented

--- views/error404.jsx
+++ views/error404.jsx
@@ -8,7 +8,7 @@ function error404 () {
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
               <div>
-                <img src="/images/e6cf1968de1b5117499c429bc37c02de.jpg" alt="Jiji with panda hat" />
+                <img className='errorCat' src="/images/e6cf1968de1b5117499c429bc37c02de.jpg" alt="Jiji with panda hat" />
                 <div>
                   Photo by <a href="https://unsplash.com/@cravethebenefits">Brandon</a> on <a href="UNSPLASH_LINK">Unsplash</a>
                 </div>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,20 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name }</h1>
+            <h2> Rating</h2>
+            <p>Currently Unrated</p>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>  
+            <form method='POST' action={`/places/${data.id}?_method=DELETE`}> 
+            <button type="submit" className="btn btn-danger">Delete</button>
+            </form> 
+            <hr width="950px;" size="5"></hr>
+            <h2>Comments!</h2>
+            <p>No Comments yet!</p>    
           </main>
         </Def>
     )
