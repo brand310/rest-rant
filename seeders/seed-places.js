67599aeba8a2e647090f1e6b245a21886345bc7e
Brandon Sandoval <105913779+brand310@users.noreply.github.com>
2022-10-06 18:23:34 -0700

    Added error messages

---
+++ seeders/seed-places.js
@@ -0,0 +1,28 @@
+const db = require("../models");
+
+db.Place.create([
+  {
+    name: "H-Thai-ML",
+    city: "Seattle",
+    state: "WA",
+    cuisines: "Thai, Pan-Asian",
+    pic: "images/emy-XoByiBymX20-unsplash.jpg",
+    founded: 1989,
+  },
+  {
+    name: "Coding Cat Cafe",
+    city: "Phoenix",
+    state: "AZ",
+    cuisines: "Coffee, Bakery",
+    pic: "images/coffee-1711012_1280.jpg",
+    founded: 2020,
+  },
+])
+  .then(() => {
+    console.log("Success!");
+    process.exit();
+  })
+  .catch((err) => {
+    console.log("Failure!", err);
+    process.exit();
+  });
