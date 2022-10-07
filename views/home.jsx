c3127d00a540e559457e9bda0997844ac7bf34a6
Brandon Sandoval <105913779+brand310@users.noreply.github.com>
2022-10-06 18:20:23 -0700

    Fixed home page code

--- views/home.jsx
+++ views/home.jsx
@@ -1,23 +1,28 @@
-const React = require('react')
-const Def = require('./default')
+const React = require("react");
+const Def = require("./default");
 
-function home () {
-    return (
-        <Def>
-            <main>
-    <h1>HOME</h1>
-    <div>
-      <img src="/images/photo-1490474504059-bf2db5ab2348.jpg" alt="Chia Fruit Shake" />
-      <div>
-        Photo by <a href="https://unsplash.com/@cravethebenefits">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
-      </div>
-    </div>
-    <a href="/places">
-        <button className="btn-primary">Places Page</button>
-    </a>
-</main>
-        </Def>
-    )
+function home() {
+  return (
+    <Def>
+      <main>
+        <h1>HOME</h1>
+        <div>
+          <img
+            src="/images/photo-1490474504059-bf2db5ab2348.jpg"
+            alt="Chia Fruit Shake"
+          />
+          <div>
+            Photo by{" "}
+            <a href="https://unsplash.com/@cravethebenefits">Brenda Godinez</a>{" "}
+            on <a href="UNSPLASH_LINK">Unsplash</a>
+          </div>
+        </div>
+        <a href="/places">
+          <button className="btn-primary">Places Page</button>
+        </a>
+      </main>
+    </Def>
+  );
 }
 
-module.exports = home
+module.exports = home;
