92ea8463a2519ce1bf10ed15cd515416628feda7
Brandon Sandoval <105913779+brand310@users.noreply.github.com>
2022-10-06 18:13:34 -0700

    Added seeded comments to show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,23 +1,64 @@
-const React = require('react')
-const Def = require('../default')
+const React = require("react");
+const Def = require("../default");
 
-function show (data) {
-    return (
-        <Def>
-          <main>
-            <h1>{ data.place.name }</h1>
-            <h2> Rating</h2>
-            <p>Currently Unrated</p>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>  
-            <form method='POST' action={`/places/${data.id}?_method=DELETE`}> 
-            <button type="submit" className="btn btn-danger">Delete</button>
-            </form> 
-            <hr width="950px;" size="5"></hr>
-            <h2>Comments!</h2>
-            <p>No Comments yet!</p>    
-          </main>
-        </Def>
-    )
+function show(data) {
+  let comments = <h3 className="inactive">No Comments yet.</h3>;
+  if (data.place.comments.length) {
+    comments = data.place.comments.map((c) => {
+      return (
+        <div className="border col-sm-4">
+          <h2 className="rant">{c.rant ? "Rant!" : "Rave!"}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <strong>- {c.author}</strong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      );
+    });
+  }
+  return (
+    <Def>
+      <main>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
+            <h1>{data.place.name}</h1>
+            <h2>Rating</h2>
+            <br />
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
+            <br />
+            <a
+              href={`/places/${data.place.id}/edit`}
+              className="btn btn-warning"
+            >
+              Edit
+            </a>
+            {` `}
+            <form
+              method="POST"
+              action={`/places/${data.place.id}?_method=DELETE`}
+            >
+              <button type="submit" className="btn btn-danger">
+                Delete
+              </button>
+            </form>
+          </div>
+        </div>
+        <hr />
+        <h2>Comments</h2>
+        <div className="row">{comments}</div>
+        <hr />
+      </main>
+    </Def>
+  );
 }
 
-module.exports = show
+module.exports = show;
