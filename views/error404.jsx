1ef69532bb523ff993a00af2239a307594adfd40
Brandon Sandoval <105913779+brand310@users.noreply.github.com>
2022-10-06 18:19:51 -0700

    Fixed error page code

--- views/error404.jsx
+++ views/error404.jsx
@@ -1,22 +1,27 @@
-const React = require('react')
-const Def = require('./default')
+const React = require("react");
+const Def = require("./default");
 
-function error404 () {
-    return (
-      <Def>
-          <main>
-              <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
-              <div>
-                <img className='errorCat' src="/images/e6cf1968de1b5117499c429bc37c02de.jpg" alt="Jiji with panda hat" />
-                <div>
-                  Photo by <a href="https://unsplash.com/@cravethebenefits">Brandon</a> on <a href="UNSPLASH_LINK">Unsplash</a>
-                </div>
-              </div>
-          </main>
-      </Def>
-    )
-  }
-  
+function error404() {
+  return (
+    <Def>
+      <main>
+        <h1>404: PAGE NOT FOUND</h1>
+        <p>Oops, sorry, we can't find this page!</p>
+        <div>
+          <img
+            className="errorCat"
+            src="/images/e6cf1968de1b5117499c429bc37c02de.jpg"
+            alt="Jiji with panda hat"
+          />
+          <div>
+            Photo by{" "}
+            <a href="https://unsplash.com/@cravethebenefits">Brandon</a> on{" "}
+            <a href="UNSPLASH_LINK">Unsplash</a>
+          </div>
+        </div>
+      </main>
+    </Def>
+  );
+}
 
-module.exports = error404
+module.exports = error404;
