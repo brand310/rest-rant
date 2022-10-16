3825dfe2794e902510fc4d465429e8ced48e598e
brand310 <brandonsandoval0@gmail.com>
2022-10-16 00:39:36 -0700

    seeded comment

--- controllers/places.js
+++ controllers/places.js
@@ -13,6 +13,15 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
+  if (req.body.pic === "") {
+    req.body.pic = undefined;
+  }
+  if (req.body.city === "") {
+    req.body.city = undefined;
+  }
+  if (req.body.state === "") {
+    req.body.state = undefined;
+  }
   db.Place.create(req.body)
     .then(() => {
       res.redirect("/places");
@@ -21,10 +30,8 @@ router.post("/", (req, res) => {
       if (err && err.name == "ValidationError") {
         let message = "Validation Error: ";
         for (var field in err.errors) {
-          message += `${field} was ${err.errors[field].value}. `;
-          message += `${err.errors[field].message}`;
+          message += `${field} was ${err.errors[field].value}. ${err.errors[field].message}\n`;
         }
-        console.log("Validation error message", message);
         res.render("places/new", { message });
       } else {
         res.render("error404");
@@ -37,7 +44,7 @@ router.get("/new", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  db.Place.findById(req.params.id)
+  db.Place.findOne(req.params.id)
     .populate("comments")
     .then((place) => {
       console.log(place.comments);
@@ -50,23 +57,76 @@ router.get("/:id", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
-  res.send("PUT /places/:id stub");
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.redirect(`/places/${req.params.id}`);
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
 });
 
 router.delete("/:id", (req, res) => {
-  res.send("DELETE /places/:id stub");
+  db.Place.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
 });
 
 router.get("/:id/edit", (req, res) => {
-  res.send("GET edit form stub");
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      res.render("places/edit", { place });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
 });
 
-router.post("/:id/rant", (req, res) => {
-  res.send("GET /places/:id/rant stub");
+router.post("/:id/comment", (req, res) => {
+  console.log("post comment", req.body);
+  if (req.body.author === "") {
+    req.body.author = undefined;
+  }
+  req.body.rant = req.body.rant ? true : false;
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      db.Comment.create(req.body)
+        .then((comment) => {
+          place.comments.push(comment.id);
+          place
+            .save()
+            .then(() => {
+              res.redirect(`/places/${req.params.id}`);
+            })
+            .catch((err) => {
+              res.render("error404");
+            });
+        })
+        .catch((err) => {
+          res.render("error404");
+        });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
 });
 
-router.delete("/:id/rant/:rantId", (req, res) => {
-  res.send("GET /places/:id/rant/:rantId stub");
+router.delete("/:id/comment/:commentId", (req, res) => {
+  db.Comment.findByIdAndDelete(req.params.commentId)
+    .then(() => {
+      console.log("Success");
+      res.redirect(`/places/${req.params.id}`);
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
 });
 
 module.exports = router;
